3e952972e588260c750ff91a026a982084ed8c83
xfiddyx <steven.woods88@hotmail.co.uk>
2020-04-01 15:25:19 +0100

    got list of kittens and puppies to show using one function

--- src/App.js
+++ src/App.js
@@ -4,6 +4,49 @@ import { Header, Hello } from './components/Header';
 import { PuppyList, handleClick } from './components/puppy.ul';
 
 const user = 'Steven';
+const puppies = [
+  {
+    name: 'Betty',
+    cuteness: 7,
+    img:
+      'https://cdn1-www.dogtime.com/assets/uploads/2020/01/golden-retriever-corgi-mixed-dog-breed-pictures-1.jpg'
+  },
+  {
+    name: 'Rebel',
+    cuteness: 8,
+    img:
+      'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12232417/Greyhound-MP.jpg'
+  },
+  {
+    name: 'Poppy',
+    cuteness: 6,
+    img:
+      'https://upload.wikimedia.org/wikipedia/commons/0/04/Labrador_Retriever_%281210559%29.jpg'
+  }
+];
+const kittens = [
+  {
+    name: 'Nori',
+    cuteness: 11,
+    img: 'https://photos.app.goo.gl/RBzgQZdqnyMZnmgc8'
+  },
+  {
+    name: 'Suki',
+    cuteness: 7,
+    img: 'https://photos.app.goo.gl/q2MFEzNbnDVrcTHE8'
+  },
+  {
+    name: 'Jiji',
+    cuteness: 10,
+    img:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/American_Shorthair.jpg/1200px-American_Shorthair.jpg'
+  },
+  {
+    name: 'Umi',
+    cuteness: 8,
+    img: 'https://photos.app.goo.gl/RBzgQZdqnyMZnmgc8'
+  }
+];
 
 function App() {
   return (
@@ -11,7 +54,7 @@ function App() {
       <Header name={user} />
       <button onClick={Hello(user)}>Click to get user</button>
       <button onClick={handleClick}>Show me the puppies!</button>
-      <PuppyList />
+      <PuppyList puppies={puppies} kittens={kittens} />
     </div>
   );
 }
--- src/components/puppy.ul.js
+++ src/components/puppy.ul.js
@@ -1,71 +1,28 @@
 import React from 'react';
 
-const puppies = [
-  {
-    name: 'Betty',
-    cuteness: 7,
-    img:
-      'https://cdn1-www.dogtime.com/assets/uploads/2020/01/golden-retriever-corgi-mixed-dog-breed-pictures-1.jpg'
-  },
-  {
-    name: 'Rebel',
-    cuteness: 8,
-    img:
-      'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12232417/Greyhound-MP.jpg'
-  },
-  {
-    name: 'Poppy',
-    cuteness: 6,
-    img:
-      'https://upload.wikimedia.org/wikipedia/commons/0/04/Labrador_Retriever_%281210559%29.jpg'
-  }
-];
-
-const kittens = [
-  {
-    name: 'Nori',
-    cuteness: 11,
-    img: 'https://photos.app.goo.gl/RBzgQZdqnyMZnmgc8'
-  },
-  {
-    name: 'Suki',
-    cuteness: 7,
-    img: 'https://photos.app.goo.gl/q2MFEzNbnDVrcTHE8'
-  },
-  {
-    name: 'Jiji',
-    cuteness: 10,
-    img:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/American_Shorthair.jpg/1200px-American_Shorthair.jpg'
-  },
-  {
-    name: 'Umi',
-    cuteness: 8,
-    img: 'https://photos.app.goo.gl/RBzgQZdqnyMZnmgc8'
-  }
-];
-
 let showImages = false;
 const handleClick = () => {
   showImages = !showImages; // <-----
 };
 
-const animals = ['kittens', 'puppies'];
-
-const PuppyList = () => {
+const PuppyList = props => {
   return (
     <ul>
-      {puppies.map(puppy => {
-        return (
-          <li key={puppy.name}>
-            <p>
-              {puppy.name}, Cuteness: {puppy.cuteness}
-            </p>
-            {showImages ? (
-              <img src={puppy.img} alt={`${puppy.name} profile pic`} />
-            ) : null}
-          </li>
-        );
+      {Object.values(props).map(value => {
+        //console.log(value);
+        return value.map(type => {
+          console.log(type.name);
+          return (
+            <li key={type.name}>
+              <p>
+                {type.name}, Cuteness: {type.cuteness}
+              </p>
+              {showImages ? (
+                <img src={type.img} alt={`${type.name} profile pic`} />
+              ) : null}
+            </li>
+          );
+        });
       })}
     </ul>
   );
